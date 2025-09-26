68aa83c80e35a4f0c5852f86e8ffc55a7e0ff55b
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-26 14:50:51 +0200

    Create edit.js

---
+++ src/modules/010-sprava-uzivatelu/forms/edit.js
@@ -0,0 +1,8 @@
+export default async function renderEditForm(root){
+  root.innerHTML = `
+    <div class="p-4 bg-white rounded-2xl border">
+      <h3 class="font-medium mb-2">Upravit uživatele</h3>
+      <p class="text-sm">Formulář (později).</p>
+    </div>
+  `;
+}
