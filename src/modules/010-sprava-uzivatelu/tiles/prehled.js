9ad297b77bc924bfe3198eca05d25625ff5e209c
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-26 14:49:47 +0200

    Add renderPrehled function for overview section

---
+++ src/modules/010-sprava-uzivatelu/tiles/prehled.js
@@ -0,0 +1,8 @@
+export default async function renderPrehled(root){
+  root.innerHTML = `
+    <div class="p-4 bg-white rounded-2xl border">
+      <h3 class="font-medium mb-2">Přehled</h3>
+      <p class="text-sm">Sem dáme grafy/karty (později).</p>
+    </div>
+  `;
+}
