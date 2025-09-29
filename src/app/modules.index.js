86dec710c6d065fb4fc7b5e9686d451797a90565
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-29 16:14:08 +0200

    Update modules.index.js

--- src/app/modules.index.js
+++ src/app/modules.index.js
@@ -1,15 +1,20 @@
-import { icon } from '../ui/icons.js';
-
-const mk = (id, title, iconName, tiles=[]) =>
-  ({ id, title, icon: icon(iconName), tiles, defaultTile: tiles[0]?.id });
-
 // src/app/modules.index.js
 // Každý nový modul = 1 řádek. Lazy import manifestu (module.config.js).
+
 export const MODULE_SOURCES = [
   () => import('../modules/010-sprava-uzivatelu/module.config.js'),
   () => import('../modules/020-muj-ucet/module.config.js'),
   () => import('../modules/030-pronajimatel/module.config.js'),
   () => import('../modules/040-nemovitost/module.config.js'),
   () => import('../modules/050-najemnik/module.config.js'),
-  // ... další přidáš stejně
+  () => import('../modules/060-smlouva/module.config.js'),
+  () => import('../modules/070-sluzby/module.config.js'),
+  () => import('../modules/080-platby/module.config.js'),
+  () => import('../modules/090-finance/module.config.js'),
+  () => import('../modules/100-energie/module.config.js'),
+  () => import('../modules/110-udrzba/module.config.js'),
+  () => import('../modules/120-dokumenty/module.config.js'),
+  () => import('../modules/130-komunikace/module.config.js'),
+  () => import('../modules/900-nastaveni/module.config.js'),
+  () => import('../modules/990-help/module.config.js'),
 ];
