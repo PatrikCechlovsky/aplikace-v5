55bd930ab49d3633adc2088a87616fb213c6f717
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-29 09:45:00 +0200

    Update prehled.js

--- src/modules/000-sablona/tiles/prehled.js
+++ src/modules/000-sablona/tiles/prehled.js
@@ -1,1 +1,13 @@
+// src/modules/000-sablona/tiles/prehled.js
+export default async function renderPrehled(root) {
+  root.innerHTML = `
+    <div class="space-y-2">
+      <h2 class="text-lg font-semibold">Přehled</h2>
+      <p class="text-slate-600 text-sm">
+        Toto je ukázková dlaždice „Přehled“. Sem patří rychlé statistiky, karty, krátké seznamy…
+      </p>
+      <div class="p-3 rounded border bg-white">Obsah přehledu…</div>
+    </div>
+  `;
+}
 
