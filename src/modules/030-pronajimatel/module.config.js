de0cb5f35a06c8caf30363f428d7b51bd71c4e8f
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-03 11:11:15 +0200

    Update module.config.js

--- src/modules/030-pronajimatel/module.config.js
+++ src/modules/030-pronajimatel/module.config.js
@@ -1,1 +1,30 @@
-t
+export async function getManifest() {
+  return {
+    id: "030-pronajimatel",      // unikátní ID
+    title: "Pronajímatel",       // název v menu
+    icon: "home",                // volitelně ikonka (podle tvého systému ikon)
+    tiles: [
+      {
+        id: "detail",
+        title: "Detail",
+        icon: "tile",
+        desc: "Formulář"
+      },
+      {
+        id: "prehled",
+        title: "Přehled",
+        icon: "list",
+        desc: "Seznam"
+      }
+    ],
+    forms: [
+      {
+        id: "novy",
+        title: "Nový / Pozvat",
+        icon: "form",
+        desc: "Formulář"
+      }
+    ],
+    defaultTile: "prehled" // volitelné, jaký je výchozí tile
+  }
+}
