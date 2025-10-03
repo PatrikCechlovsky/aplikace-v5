78fff2a2bc4edef156f47d85e528fba45ec0cfd9
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-03 10:07:01 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,6 @@ import { renderCommonActions } from './ui/commonActions.js';
 import { renderDashboardTiles, loadFavorites, setFavorite } from './ui/content.js';
 
 
-// Přidej tyto dva řádky:
-import Sortable from 'sortablejs';
-window.Sortable = Sortable;
-
 // ========== Mini utils ==========
 const $ = (sel) => document.querySelector(sel);
 const $id = (id) => document.getElementById(id);
