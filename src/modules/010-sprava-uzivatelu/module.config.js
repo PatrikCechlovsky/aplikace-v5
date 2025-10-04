8a5d5aa45fb9543ddca3de342c20da1e4ecac05b
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-04 19:32:41 +0200

    Add new form entry to module configuration

--- src/modules/010-sprava-uzivatelu/module.config.js
+++ src/modules/010-sprava-uzivatelu/module.config.js
@@ -7,9 +7,10 @@ export async function getManifest() {
       { id: 'prehled', title: 'Přehled', icon: 'list', renderer: () => import('./tiles/prehled.js') }
     ],
     forms: [
-      { id: 'read', title: 'Detail', renderer: () => import('./forms/form.js') },
-      { id: 'edit', title: 'Upravit', renderer: () => import('./forms/form.js') },
-      { id: 'create', title: 'Nový / Pozvat', renderer: () => import('./forms/form.js') }
+      { id: 'read',    title: 'Detail',          renderer: () => import('./forms/form.js') },
+      { id: 'edit',    title: 'Upravit',         renderer: () => import('./forms/form.js') },
+      { id: 'create',  title: 'Nový / Pozvat',   renderer: () => import('./forms/form.js') },
+      { id: 'form',    title: 'Formulář',        renderer: () => import('./forms/form.js') }
     ],
     defaultTile: 'prehled',
   };
