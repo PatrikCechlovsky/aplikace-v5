e53673db378dcb6f6541753b2ce47c222e6e8bdb
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-04 20:02:54 +0200

    Update module.config.js

--- src/modules/010-sprava-uzivatelu/module.config.js
+++ src/modules/010-sprava-uzivatelu/module.config.js
@@ -1,16 +1,14 @@
-import { icon } from '../../ui/icons.js';
-
 export async function getManifest() {
   return {
     id: '010-sprava-uzivatelu',
     title: 'Uživatelé',
     icon: 'users',
     tiles: [
-      { id: 'prehled', title: 'Přehled', icon: icon('list'), renderer: () => import('./tiles/prehled.js') }
+      { id: 'prehled', title: 'Přehled', icon: 'list', renderer: () => import('./tiles/prehled.js') }
     ],
     forms: [
-      { id: 'create',  title: 'Nový / Pozvat', icon: icon('add'), renderer: () => import('./forms/create.js') },
-      { id: 'form',    title: 'Formulář',      icon: icon('edit'), renderer: () => import('./forms/form.js') }
+      { id: 'create',  title: 'Nový / Pozvat', icon: 'add', renderer: () => import('./forms/create.js') },
+      { id: 'form',    title: 'Formulář',      icon: 'edit', renderer: () => import('./forms/form.js') }
     ],
     defaultTile: 'prehled',
   };
