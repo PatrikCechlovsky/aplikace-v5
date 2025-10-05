e8cb06a84ac7239179b32f18a49aba86ceb3de09
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-05 10:59:14 +0200

    Refactor tile and form definitions for clarity

--- src/modules/010-sprava-uzivatelu/module.config.js
+++ src/modules/010-sprava-uzivatelu/module.config.js
@@ -1,15 +1,37 @@
+// src/modules/010-sprava-uzivatelu/module.config.js
 export async function getManifest() {
   return {
     id: '010-sprava-uzivatelu',
     title: 'Uživatelé',
     icon: 'users',
+
+    // Dlaždice (tiles) – lazy import (renderer je funkce)
     tiles: [
-      { id: 'prehled', title: 'Přehled', icon: 'list', renderer: () => import('./tiles/prehled.js') }
+      {
+        id: 'prehled',
+        title: 'Přehled',
+        icon: 'list',
+        renderer: () => import('./tiles/prehled.js')
+      }
     ],
+
+    // Formuláře (forms) – lazy import
     forms: [
-      { id: 'create',  title: 'Nový / Pozvat', icon: 'add', renderer: () => import('./forms/create.js') },
-      { id: 'form',    title: 'Formulář',      icon: 'edit', renderer: () => import('./forms/form.js') }
+      {
+        id: 'create',
+        title: 'Nový / Pozvat',
+        icon: 'add',
+        renderer: () => import('./forms/create.js')
+      },
+      {
+        id: 'form',
+        title: 'Formulář',
+        icon: 'edit',
+        renderer: () => import('./forms/form.js')
+      }
     ],
+
+    // výchozí sekce pro /m/010-sprava-uzivatelu
     defaultTile: 'prehled',
   };
 }
