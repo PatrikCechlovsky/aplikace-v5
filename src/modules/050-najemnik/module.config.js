a1e75f7ad0b746416183cbaeb83a364bc0c65b36
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-18 11:37:21 +0200

    Add getManifest function to module.config.js

--- src/modules/050-najemnik/module.config.js
+++ src/modules/050-najemnik/module.config.js
@@ -1,1 +1,15 @@
-t
+export async function getManifest() {
+  return {
+    id: '050-najemnik',
+    title: 'Nájemník',
+    icon: 'users',
+    defaultTile: 'prehled',
+    tiles: [
+      { id: 'prehled', title: 'Přehled', icon: 'list' },
+      { id: 'seznam', title: 'Seznam', icon: 'list' }
+    ],
+    forms: [
+      { id: 'form', title: 'Formulář', icon: 'form' }
+    ]
+  };
+}
