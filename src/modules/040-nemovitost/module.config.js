9f568897831ded90d04329a29f8eb61431d2d7e8
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-10-24 17:00:26 +0200

    Update module.config.js

--- src/modules/040-nemovitost/module.config.js
+++ src/modules/040-nemovitost/module.config.js
@@ -13,11 +13,11 @@ export async function getManifest() {
       // All functionality consolidated into 'prehled' with type filtering
     ],
     forms: [
-      { id: 'chooser', title: 'Výběr typu nemovitosti', icon: 'grid', showInSidebar: false },
-      { id: 'edit', title: 'Úprava / Nová nemovitost', icon: 'edit', showInSidebar: false },
+     //  { id: 'chooser', title: 'Výběr typu nemovitosti', icon: 'grid', showInSidebar: false },
+      { id: 'edit', title: 'Nová nemovitost', icon: 'add', showInSidebar: false },
       { id: 'detail', title: 'Detail nemovitosti', icon: 'eye', showInSidebar: false },
       { id: 'unit-chooser', title: 'Výběr typu jednotky', icon: 'grid', showInSidebar: false },
-      { id: 'unit-edit', title: 'Úprava / Nová jednotka', icon: 'edit', showInSidebar: false },
+      { id: 'unit-edit', title: 'Nová jednotka', icon: 'add', showInSidebar: false },
       { id: 'property-type', title: 'Správa typů nemovitostí', icon: 'settings', showInSidebar: false },
       { id: 'unit-type', title: 'Správa typů jednotek', icon: 'settings', showInSidebar: false },
     ],
